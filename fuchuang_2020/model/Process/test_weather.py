2c7ec3c688c2d653731d5d7c2d83c4656b0224d7
trdthg <trdthg@outlook.com>
2022-02-08 23:50:19 +0800

    :fire: remove files
    Signed-off-by: trdthg <trdthg@outlook.com>

--- fuchuang_2020/model/Process/test_weather.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-a = pd.DataFrame(pd.read_csv('./data/weather.csv', encoding='gbk'))
-b = a['天气状况']
-b = set(b)
-c = []
-for a in b:
-    c.extend(a.split(' /'))
-print(set(c))
