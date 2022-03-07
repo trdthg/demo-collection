186c1bdf0dbc79ccef553130e85853593bb97ae4
trdthg <trdthg@outlook.com>
2022-03-07 12:49:39 +0800

    :fix: 人名可以重复;
    Signed-off-by: trdthg <trdthg@outlook.com>

--- src/main/java/com/moflowerlkh/decisionengine/entity/User.java
+++ src/main/java/com/moflowerlkh/decisionengine/entity/User.java
@@ -26,7 +26,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name", nullable = false, unique = true)
+    @Column(name = "name", nullable = false)
     @Length(min = 1, max = 64)
     private String name;
 
