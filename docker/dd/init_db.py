1f99f951644e0dfbd23b6b3e672c7ff9fdea4ab2
trdthg <trdthg@outlook.com>
2022-04-17 12:57:45 +0800

    :sparkles: dd 文件夹bug
    Signed-off-by: trdthg <trdthg@outlook.com>

---
+++ docker/dd/init_db.py
@@ -0,0 +1,224 @@
+from datetime import datetime
+import csv
+
+from random import randbytes, randint
+
+# import faker
+import pymysql
+
+_default_host = "127.0.0.1"
+_default_port = 3306
+
+
+def generate_random():
+
+    f = open("data/users.csv", "w")
+    userwriter = csv.writer(f, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
+    f = open("data/accounts.csv", "w")
+    accountwriter = csv.writer(f, delimiter=',',
+                               quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
+    f = open("data/roles.csv", "w")
+    rolewriter = csv.writer(f, delimiter=',',
+                            quotechar='|', quoting=csv.QUOTE_MINIMAL)
+
+    n = 10000
+
+    for i in range(0, n+1):
+        id = 10000+i
+        create_time = "2022-04-16 00:00:00.000000"
+        modify_time = "2022-04-16 00:00:00.000000"
+        balance = 1000000
+        bank_account_sn = id
+        user_id = id
+        accountwriter.writerow([id,
+                                create_time,
+                                modify_time,
+                                balance,
+                                bank_account_sn,
+                                user_id])
+
+    #     create table user_roles
+    for i in range(0, n + 1):
+        user_id = 10000+i
+        roles = "test"
+        rolewriter.writerow([user_id, roles])
+
+    for i in range(0, n + 1):
+        # id          bigint auto_increment primary key, 100000 + i
+        id = 10000 + i
+        create_time = "2022-04-16 00:00:00.000000"
+        modify_time = "2022-04-16 00:00:00.000000"
+        idnumber = str(id) + "idnumber"
+        age = randint(20, 50)
+        country = "中国"
+        dishonest = False
+        employment = "Employed"
+        gender = "Male"
+        name = str(randbytes(10))
+        over_dual = 0
+        password = "$2a$10$A77LByAaUQrDUHjz61.eKO3baRgsCByiaH0aKAYSgYJ5RvNoIoxkm"
+        username = "user" + str(id)
+        year_income = 100000
+        userwriter.writerow([id, create_time, modify_time, idnumber,
+                            age,
+                            country,
+                            dishonest,
+                            employment,
+                            gender,
+                            name,
+                            over_dual,
+                            password,
+                            username,
+                            year_income
+                             ])
+
+
+class MYSQL:
+    def __init__(self, user, pwd, host=_default_host, port=_default_port):
+        self.host = host
+        self.port = port
+        self.user = user
+        self.__pwd = pwd
+        self.db = pymysql.connect(
+            host=self.host,
+            port=self.port,
+            user=self.user,
+            password=self.__pwd,
+            local_infile=1
+        )
+        self.__drop_table("user_roles")
+        self.__drop_table("bank_account")
+        self.__drop_table("user")
+        self.__create_tables()
+
+    def connect_database(self, database_name):
+        self.db.db = database_name
+
+    def load_user_data(self, filepath, table):
+        # self.__drop_table(table)
+        # self.__create_tables()
+        start = datetime.now()
+        sql = """
+        LOAD DATA LOCAL INFILE '%s' INTO TABLE ms.%s FIELDS TERMINATED BY ',' (
+            id, create_time, modify_time, idnumber,
+                         age,
+                         country,
+                         dishonest,
+                         employment,
+                         gender,
+                         name,
+                         over_dual,
+                         password,
+                         username,
+                         year_income
+        );
+        """
+        self.execute_sql(sql % (filepath, table))
+        print("load data finish after", datetime.now() - start)
+
+    def load_roles_data(self, filepath, table):
+        # self.__drop_table(table)
+        # self.__create_tables()
+        start = datetime.now()
+        sql = """
+        LOAD DATA LOCAL INFILE '%s' INTO TABLE ms.%s FIELDS TERMINATED BY ',' (
+            user_id, roles
+        );
+        """
+        self.execute_sql(sql % (filepath, table))
+        print("load data finish after", datetime.now() - start)
+
+    def load_account_data(self, filepath, table):
+        # self.__create_tables()
+        start = datetime.now()
+        sql = """
+        LOAD DATA LOCAL INFILE '%s' INTO TABLE ms.%s FIELDS TERMINATED BY ',' (
+            id,
+                            create_time,
+                            modify_time,
+                            balance,
+                            bank_account_sn,
+                            user_id
+        );
+        """
+        self.execute_sql(sql % (filepath, table))
+        print("load data finish after", datetime.now() - start)
+
+    def execute_sql(self, sql):
+        try:
+            self.db.cursor().execute(sql)
+            self.db.commit()
+        except pymysql.Error as e:
+            print("error in execute_sql:", e)
+            # print("sql:", sql)
+
+    def __drop_table(self, table):
+        print("drop")
+        sql = """
+        DROP TABLE IF EXISTS ms.%s;
+        """
+        self.execute_sql(sql % table)
+        print("drop success")
+
+    def __create_tables(self):
+        sql = """
+        CREATE TABLE ms.user (
+            `id` bigint(20) NOT NULL AUTO_INCREMENT,
+            `create_time` datetime(6) DEFAULT NULL,
+            `modify_time` datetime(6) DEFAULT NULL,
+            `idnumber` varchar(255) NOT NULL,
+            `age` int(11) NOT NULL,
+            `city` varchar(255) DEFAULT NULL,
+            `country` varchar(255) DEFAULT NULL,
+            `dishonest` bit(1) DEFAULT NULL,
+            `employment` varchar(255) DEFAULT NULL,
+            `gender` varchar(255) NOT NULL,
+            `name` varchar(64) NOT NULL,
+            `over_dual` bigint(20) DEFAULT NULL,
+            `password` varchar(255) NOT NULL,
+            `province` varchar(255) DEFAULT NULL,
+            `username` varchar(255) NOT NULL,
+            `year_income` bigint(20) DEFAULT NULL,
+            PRIMARY KEY (`id`),
+            UNIQUE KEY `UK_tgkwrgg3qouq6087dy4schx61` (`idnumber`),
+            UNIQUE KEY `UK_sb8bbouer5wak8vyiiy4pf2bx` (`username`)
+        ) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=utf8mb4;
+        """
+        self.execute_sql(sql)
+
+        sql = '''
+        CREATE TABLE ms.user_roles (
+            `user_id` bigint(20) NOT NULL,
+            `roles` varchar(255) DEFAULT NULL,
+            KEY `FK55itppkw3i07do3h7qoclqd4k` (`user_id`),
+            CONSTRAINT `FK55itppkw3i07do3h7qoclqd4k` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+        '''
+        self.execute_sql(sql)
+
+        sql = '''
+        CREATE TABLE ms.bank_account (
+            `id` bigint(20) NOT NULL AUTO_INCREMENT,
+            `create_time` datetime(6) DEFAULT NULL,
+            `modify_time` datetime(6) DEFAULT NULL,
+            `balance` bigint(20) NOT NULL,
+            `bank_account_sn` bigint(20) NOT NULL,
+            `user_id` bigint(20) NOT NULL,
+            PRIMARY KEY (`id`),
+            UNIQUE KEY `UK_sufktt3h6rerqjo7kxrogmr30` (`bank_account_sn`)
+        ) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=utf8mb4;
+        '''
+        self.execute_sql(sql)
+
+
+if __name__ == '__main__':
+    # generate_random()
+    db = MYSQL('root', 'ms', host='localhost')
+    db.connect_database('ms')
+
+    db.load_user_data('data/users.csv', "user")
+    db.load_roles_data('data/roles.csv', "user_roles")
+    db.load_account_data('data/accounts.csv', "bank_account")
---
+++ docker/dd/script.py
@@ -0,0 +1,99 @@
+
+import redis
+import csv
+import requests
+# from flask import Flask
+import faker
+
+
+def login_users(n):
+
+    with open('tokens.csv', 'w', newline='') as csvfile:
+        spamwriter = csv.writer(csvfile, delimiter=',',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        spamwriter.writerow(["user_id", "token", "account_id"])
+        for i in range(1, n + 1):
+            print(i, end=" ")
+            res = requests.post("http://tguio.club:8848/api/auth/signin", json={
+                "username": "user"+str(i + 10010),
+                "password": "000000"
+            })
+            content = res.json()
+            if content["code"] == 200:
+                token = content["content"]["token"]
+                user_id = content["content"]["user_id"]
+                account_id = content["content"]["account_id"][0]
+                spamwriter.writerow([user_id, token, account_id])
+
+
+def clear_redis():
+
+    r = redis.StrictRedis(host='tguio.club', port=6379,
+                          db=0, password="Aa1@0000")
+    r.flushall()
+
+
+def admin_login(username):
+    res = requests.post("http://tguio.club:8848/api/auth/signin", json={
+        "username": username,
+        "password": "000000"
+    })
+    res = res.json()
+    print(res)
+    return res["content"]["token"]
+
+
+def create_new_activity(token):
+    res = requests.post("http://tguio.club:8848/api/loan/",
+                        headers={
+                            "Authorization": token
+                        },
+                        json={
+                            "activity_apr": 1.0,
+                            "activity_endTime": "2008-12-12 23:12:1",
+                            "activity_initMoney": 10,
+                            "activity_moneyLimit": 10000000,
+                            "activity_name": "adwa",
+                            "activity_replayTime": 10,
+                            "activity_startTime": "2008-12-12 23:12:1",
+                            "activity_timeLimit": "3/6",
+
+                            "activity_totalAmount": 10 * 10000 * 1,
+
+                            "activity_totalQuantity": 1800,
+                            "activity_perPrice": 100,
+                            "activity_oneMaxAmount": 1,
+
+                            "rule": {
+                                "activity_ageFloor": 0,
+                                "activity_ageUp": 100,
+                                "activity_checkDishonest": False,
+                                "activity_checkNation": True,
+                                "activity_checkOverdual": True,
+                                "activity_checkwork": True,
+                                "activity_guarantee": True,
+                                "activity_pledge": True
+                            }
+                        })
+
+    res = res.json()
+    print(res)
+    activity_id = res["content"]["activity_id"]
+    return activity_id
+
+
+if __name__ == "__main__":
+    # app = Flask("name")
+
+    # @app.route('/hello')
+    # def hello():
+    #     return {
+    #         "activity_id": newid
+    #     }
+    # app.run()
+
+    print("start...")
+    # clear_redis()
+    login_users(10)
+    newid = create_new_activity(admin_login("user10005"))
+    print(newid)
